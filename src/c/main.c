a719fbd360ed85b4c6b6ebeb6e0bf14a6ea30eec
Steven Go <greenlover1991@yahoo.com>
2018-04-07 09:01:35 +0800

    trial on nested actions
    TODO:
    - send nested actions via bluetooth (a bit clunky)
    - or send a flatten stack via string manipulation
    
    will do a 1 level action - comma-separated simple arrays instead

--- src/c/main.c
+++ src/c/main.c
@@ -8,6 +8,7 @@ typedef struct Action {
   char *payload_on_click;
   bool input_payload;
   struct Action* child_actions;
+  bool grid_layout;
   int child_actions_count;
 } Action;
 
@@ -47,6 +48,7 @@ static void free_actions(Action* actions, int count) {
   for (i = 0; i < count; i++) {
     free_action(actions[i]);
   }
+  free(actions);
 }
 
 static void free_flows() {
@@ -85,6 +87,18 @@ static char* strdup(char* from) {
   return result;
 }
 
+/* Utility method: split string in a loop */
+static char *strsep(char **stringp, const char *delim) {
+  if (*stringp == NULL) { return NULL; }
+  char *token_start = *stringp;
+  *stringp = strpbrk(token_start, delim);
+  if (*stringp) {
+    **stringp = '\0';
+    (*stringp)++;
+  }
+  return token_start;
+}
+
 /* Utility method: construct flow from app message */
 static Flow construct_flow(DictionaryIterator *iter) {
   Flow flow;
@@ -93,6 +107,33 @@ static Flow construct_flow(DictionaryIterator *iter) {
   flow.is_running = dict_find(iter, MESSAGE_KEY_is_running)->value->int32 == 1;
   flow.action_count = 0;
   return flow;
+}
+
+
+void action_performed(ActionMenu *action_menu, const ActionMenuItem *action, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "ACTION performed");
+  // FIXME call this in on close instead
+  action_menu_hierarchy_destroy(action_menu_get_root_level(action_menu), NULL, NULL);
+}
+
+static ActionMenuLevel* construct_level(Action* actions, int action_count) {
+  ActionMenuLevel* level = action_menu_level_create(action_count);
+  int i;
+  for (i = 0; i < action_count; i++) {
+    Action action = actions[i];
+    if (action.child_actions_count <= 0) {
+      // if no children, add as an action
+      action_menu_level_add_action(level, action.name, action_performed, NULL);
+    } else {
+      // if with children, recurse and add as level
+      ActionMenuLevel* child = construct_level(action.child_actions, action.child_actions_count);
+      if (action.grid_layout) {
+        action_menu_level_set_display_mode(child, ActionMenuLevelDisplayModeThin);  
+      }
+      action_menu_level_add_child(level, child, action.name);
+    }
+  }
+  return level;
 }
 
 static uint16_t get_num_rows(struct MenuLayer* menu_layer, uint16_t section_index, void *callback_context) {
@@ -105,9 +146,43 @@ static void draw_row(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_ind
 }
 
 static void select_click(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
-  // FIXME sgo send app message to start flow
-  Flow flow = s_flows[cell_index->row];
-  comms_send_params(REQ_TRIGGER_FLOW, flow.id);
+//   Flow flow = s_flows[cell_index->row];
+//   comms_send_params(REQ_TRIGGER_FLOW, flow.id);
+  
+  // FIXME destroy root level
+  ActionMenuLevel* root_level = action_menu_level_create(2);
+  
+  ActionMenuLevel* wide_level = action_menu_level_create(4);
+  action_menu_level_add_action(wide_level, "Action 1", action_performed, NULL);
+  action_menu_level_add_action(wide_level, "Action 2", action_performed, NULL);
+  action_menu_level_add_action(wide_level, "Action 3", action_performed, NULL);
+  action_menu_level_add_action(wide_level, "Action 4", action_performed, NULL);
+  
+  ActionMenuLevel* grid_level = action_menu_level_create(7);
+  action_menu_level_set_display_mode(grid_level, ActionMenuLevelDisplayModeThin);
+  action_menu_level_add_action(grid_level, "G 1", action_performed, NULL);
+  action_menu_level_add_action(grid_level, "G 2", action_performed, NULL);
+  action_menu_level_add_action(grid_level, "G 3", action_performed, NULL);
+  action_menu_level_add_action(grid_level, "GSI 4", action_performed, NULL);
+  action_menu_level_add_action(grid_level, "G 5", action_performed, NULL);
+  action_menu_level_add_action(grid_level, "Gwas 6", action_performed, NULL);
+  action_menu_level_add_action(grid_level, "G 7", action_performed, NULL);
+
+  // add the levels
+  action_menu_level_add_child(root_level, wide_level, "This is wide level");
+  action_menu_level_add_child(root_level, grid_level, "Grid level");
+  
+  // FIXME construct actions
+  ActionMenuConfig config = (ActionMenuConfig){
+    .root_level = root_level,
+    .context = NULL,
+    //.colors = (),
+    .will_close = NULL,
+    .did_close = NULL,
+    .align = ActionMenuAlignCenter
+  };
+  ActionMenu* action_menu = action_menu_open(&config);
+  
 }
 
 static void refresh_menu_layer() {
@@ -149,6 +224,14 @@ static void inbox_received_handler(DictionaryIterator *iter, void *context) {
     // construct items if still receiving
     } else if (s_bool_receiving) {
       s_flows[s_flow_index] = construct_flow(iter);
+      
+      
+      // FIXME testing hardcoded actions
+      if (s_flow_index == 0) {
+        
+  
+        strsep(&s_flows[0].name, ",");
+      }
 
       s_flow_index++;
     }
